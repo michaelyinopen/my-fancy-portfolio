a5f05f0d98d50d9cbe81fcd4335a33d500fc5f53
michaelyinopen <michaelyinopen@gmail.com>
2021-11-01 19:21:17 +1100

    concept art sky

--- pages/concept-art.js
+++ pages/concept-art.js
@@ -4,16 +4,23 @@ import styles from '../styles/ConceptArt.module.css'
 
 export default function ConceptArt() {
   return (
-    <div className={styles.page}>
+    <div>
       <Head>
         <meta name="description" content="Michael Yin's Programmer Portfolio Concept Art" />
       </Head>
-      <div className={styles.landingScreen}>
-        <Link href="/" scroll={false}>
-          <a className={styles.card}>
-            <h2>&larr; Portfolio</h2>
-          </a>
-        </Link>
+      <Link href="/" scroll={false}>
+        <a className={styles.card}>
+          <h2>&larr; Portfolio</h2>
+        </a>
+      </Link>
+      <div className={styles.page}>
+        <div className={styles.portion}>
+          <div className={styles.sky} />
+        </div>
+        <div className={styles['sea-surface']} />
+        <div className={styles['shallow-sea']} />
+        <div className={styles['deep-sea']} />
+        <div className={styles['seabed']} />
       </div>
     </div>
   )
